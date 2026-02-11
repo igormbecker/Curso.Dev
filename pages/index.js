a9eb0d7014422e91c06410b4675e9fdeac37be1d
Igor Becker <igormbecker@gmail.com>
2026-02-11 17:05:33 -0300

    Melhorando a mensagem.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
 function Home() {
-    return <h1>Parabéns pelo Clínicas ter te chamado mozão! Te amo, vai dar tudo certo e tmj!</h1>
+    return (
+        <div style={{ 
+            display: "flex", 
+            justifyContent: "center", 
+            alignItems: "center", 
+            height: "100vh",
+            background: "linear-gradient(135deg, #0f2027, #203a43, #2c5364)",
+            color: "white",
+            textAlign: "center",
+            padding: "20px"
+        }}>
+            <h1 style={{ fontSize: "2rem" }}>
+                🎉 Parabéns pelo Clínicas ter te chamado, mozão! 💙<br/><br/>
+                Eu tenho muito orgulho de você 😍<br/>
+                Vai dar tudo certo 🙏✨<br/><br/>
+                Tmj sempre 🚀❤️
+            </h1>
+        </div>
+    );
 }
 
 export default Home;
