018e98cfe98a0f4c34f3aaa5482f2df58a48759e
Igor Becker <igormbecker@gmail.com>
2026-02-14 10:07:22 -0300

    adicionando padrao de projeto

--- README.md
+++ README.md
@@ -1,31 +1,55 @@
 # Curso.Dev
+
 Projeto desenvolvido no curso.dev que clone o projeto do site www.tabnews.com.br
 
 # Frameworks instalados
+
 - Node: lts/hydrogen
 - NextJs: next@13.1.6
 - React: react@18.2.0
 - React-Dom: react-dom@18.2.0
 
-# Comandos utilizados para rodar o projeto local: 
-- listar versões do node: 
-``` bash
+# Comandos utilizados para rodar o projeto local:
+
+- listar versões do node:
+
+```bash
 nvm ls
 ```
-- para instalar o node: 
-``` bash
+
+- para instalar o node:
+
+```bash
 nvm install lts/hydrogen
 ```
-- Para instalar dependencias: 
-``` bash
+
+- Para instalar dependencias:
+
+```bash
 npm install next@13.1.6 react@18.2.0 react-dom@18.2.0
 ```
+
 - Abra o power shell no windows e clique na seta > e clique em linux terminal.
 - Localizar a pasta /home/igormbecker/curso.dev/Curso.Dev no terminal do linux e execute o comando a seguir:
+
 ```bash
 code .
 ```
-- Para rodar o projeto: 
-``` bash
+
+- Para rodar o projeto:
+
+```bash
 npm run dev
+```
+
+- Para rodar o script que verifica padrão do código de todo projeto:
+
+```bash
+npm run lint:check
+```
+
+- para rodar o script que padroniza o código de todo projeto:
+
+```bash
+npm run lint:fix
 ```
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,34 @@
 function Home() {
-    return (
-        <div style={{ 
-            display: "flex", 
-            justifyContent: "center", 
-            alignItems: "center", 
-            height: "100vh",
-            background: "linear-gradient(135deg, #0f2027, #203a43, #2c5364)",
-            color: "white",
-            textAlign: "center",
-            padding: "20px"
-        }}>
-            <h1 style={{ fontSize: "2rem" }}>
-                🎉 Parabéns pelo Clínicas ter te chamado, mozão! 💙<br/><br/>
-                Eu tenho muito orgulho de você 😍<br/>
-                Vai dar tudo certo 🙏✨<br/><br/>
-                Tmj sempre 🚀❤️
-            </h1>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        background: "linear-gradient(135deg, #0f2027, #203a43, #2c5364)",
+        color: "white",
+        textAlign: "center",
+        padding: "20px",
+      }}
+    >
+      <h1 style={{ fontSize: "2rem" }}>
+        🎉 Parabéns pelo Clínicas ter te chamado, mozão! 💙
+        <br />
+        <br />
+        Eu tenho muito orgulho de você 😍
+        <br />
+        Vai dar tudo certo 🙏✨
+        <br />
+        <br />
+        Tmj sempre 🚀❤️
+      </h1>
+    </div>
+  );
+}
+
+function Test() {
+  console.log("Test function executed");
 }
 
 export default Home;
