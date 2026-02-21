91bc9dc1ebcab9a26f7d8d0bb05d9a31e8fda3be
Igor Becker <igormbecker@gmail.com>
2026-02-20 21:59:50 -0300

    adicionando os testes

---
+++ models/calculadora.js
@@ -0,0 +1,7 @@
+﻿function somar(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") return "Error";
+
+  return a + b;
+}
+
+exports.somar = somar;
--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,3 +1,21 @@
-﻿test("nome do teste", () => {
-  expect(1).toBe(1);
+﻿const calculadora = require("../models/calculadora.js");
+
+test("soma de 1 + 2 deve ser igual a 3", () => {
+  const result = calculadora.somar(1, 2);
+  expect(result).toBe(3);
+});
+
+test("soma de 2 + 2 deve ser igual a 4", () => {
+  const result = calculadora.somar(2, 2);
+  expect(result).toBe(4);
+});
+
+test("soma de 5 + 100 deve ser igual a 105", () => {
+  const result = calculadora.somar(5, 100);
+  expect(result).toBe(105);
+});
+
+test("soma 'banana' + 100 deve ser igual a 'Erro'", () => {
+  const result = calculadora.somar("banana", 100);
+  expect(result).toBe("Error");
 });
